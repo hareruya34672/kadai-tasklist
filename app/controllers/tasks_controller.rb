28074b4a2070088b093306f3bce36fc1df9e33ed
Hiro <romann5744@yahoo.co.jp>
2020-06-25 16:03:51 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  
   def index
     if logged_in?
       @tasks = current_user.tasks.all.page(params[:page])
